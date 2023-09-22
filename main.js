53b6bf4de9877bf631131fae7254c4056406c9e3
ellyhp <ellygmr_13@hotmail.com>
2023-09-21 19:58:51 -0600

    v2

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 
 
-    const btnUnshift = document.getElementById('btn-unshift');
-    const contentUnshift = document.getElementById('content-unshift');
-    const btnUnshiftSelect = document.getElementById('btn-unshift-select');
+    ModalUnshift();
 
-        btnUnshift.addEventListener('click', () => {
-            contentUnshift.classList.add('hidden');
-        });
 
-        btnUnshiftSelect.addEventListener('click', () => {
-            contentUnshift.classList.remove('hidden')
-        });
+    function ModalUnshift(){
+        const btnUnshift = document.getElementById('btn-unshift');
+        const contentUnshift = document.getElementById('content-unshift');
+        const btnUnshiftSelect = document.getElementById('btn-unshift-select');
+    
+            btnUnshift.addEventListener('click', () => {
+                contentUnshift.classList.add('hidden');
+            });
+    
+            btnUnshiftSelect.addEventListener('click', () => {
+                contentUnshift.classList.remove('hidden')
+            });
+
+            let ArrayUnshift = ['🍔', '🍕', '🌭'];
+            let ApplyArrayMethod = ArrayUnshift.unshift('🍟');
+            console.log(ArrayUnshift);
+
+    }
+
+    
+
+
